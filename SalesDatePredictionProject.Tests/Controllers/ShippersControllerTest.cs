423ee3a28e2c9df3caa69d2699200851f17f692f
PaoM21 <pao_951113@hotmail.com>
2025-01-19 17:00:02 -0500

    Add tests Controllers and Repository.

---
+++ SalesDatePredictionProject.Tests/Controllers/ShippersControllerTest.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using SalesDatePredictionProject.Server.Controllers;
+using SalesDatePredictionProject.Server.Dto;
+using SalesDatePredictionProject.Server.Interfaces;
+using SalesDatePredictionProject.Server.Models;
+
+namespace SalesDatePredictionProject.Tests.Controllers
+{
+    public class ShippersControllerTest
+    {
+        private IShippersRepository _shippersRepository { get; set; }
+        private IMapper _mapper { get; set; }
+        private ShippersController _shippersController { get; set; }
+        public ShippersControllerTest()
+        {
+            //Dependencies
+            _mapper = A.Fake<IMapper>();
+            _shippersRepository = A.Fake<IShippersRepository>();
+
+            //SUT
+            _shippersController = new ShippersController(_shippersRepository, _mapper);
+        }
+
+        [Fact]
+        public void ShippersController_GetShippers_ReturnsSuccess()
+        {
+            // Arrange
+            var shippers = A.Fake<ICollection<ShipperDto>>();
+            var shippersModel = A.Fake<ICollection<Shippers>>();
+            A.CallTo(() => _shippersRepository.GetShippers()).Returns(shippersModel);
+            A.CallTo(() => _mapper.Map<ICollection<ShipperDto>>(shippersModel)).Returns(shippers);
+
+            // Act
+            var result = _shippersController.Get();
+
+            // Assert
+            result.Should().BeOfType<OkObjectResult>();
+        }
+    }
+}
---
+++ SalesDatePredictionProject.Tests/Repository/OrdersRepositoryTest.cs
@@ -0,0 +1,111 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using SalesDatePredictionProject.Server.Data;
+using SalesDatePredictionProject.Server.Models;
+using SalesDatePredictionProject.Server.Repository;
+
+namespace SalesDatePredictionProject.Tests.Repository
+{
+    public class OrdersRepositoryTest
+    {
+        private async Task<DataContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            var databaseContext = new DataContext(options);
+            databaseContext.Database.EnsureCreated();
+
+            if (await databaseContext.Orders.CountAsync() < default(int))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    databaseContext.Orders.Add(
+                        new Orders()
+                        {
+                            OrderId = 1,
+                            CustId = 1,
+                            EmpId = 1,
+                            OrderDate = DateTime.Now,
+                            RequiredDate = DateTime.Now,
+                            ShippedDate = DateTime.Now,
+                            ShipperId = 1,
+                            Freight = 10,
+                            ShipName = "Name",
+                            ShipAddress = "Address",
+                            ShipCity = "City",
+                            ShipRegion = "Region",
+                            ShipPostalCode = "PostalCode",
+                            ShipCountry = "Country"
+                        });
+
+                    await databaseContext.SaveChangesAsync();
+                }
+            }
+
+            return databaseContext;
+        }
+
+        [Fact]
+        public async void OrdersRepository_GetOrdersByCustom_ReturnsICollection()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var ordersRepository = new OrdersRepository(dbContext);
+            int custId = 1;
+
+            //Act
+            var result = ordersRepository.GetOrdersByCustom(custId);
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().AllBeOfType<Orders>();
+            result.Should().BeOfType<ICollection<Orders>>();
+        }
+
+        [Fact]
+        public async void OrdersRepository_GetOrders_ReturnsICollection()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var ordersRepository = new OrdersRepository(dbContext);
+
+            //Act
+            var result = ordersRepository.GetOrders();
+
+            //Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType<ICollection<Orders>>();
+        }
+
+        [Fact]
+        public async void OrdersRepository_CustomerExists_ReturnsBool()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var ordersRepository = new OrdersRepository(dbContext);
+            int custId = 1;
+
+            //Act
+            var result = ordersRepository.CustomerExists(custId);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public async void OrdersRepository_OrderExists_ReturnsBool()
+        {
+            //Arrange
+            var dbContext = await GetDbContext();
+            var ordersRepository = new OrdersRepository(dbContext);
+            int orerId = 1;
+
+            //Act
+            var result = ordersRepository.OrderExists(orerId);
+
+            //Assert
+            result.Should().BeTrue();
+        }
+    }
+}
