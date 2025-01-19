fded0cb6c289e33ae27a92590c13d64139d1c6a4
PaoM21 <pao_951113@hotmail.com>
2025-01-19 16:58:20 -0500

    Create Dto's.

---
+++ SalesDatePredictionProject.Server/Dto/EmployeeDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SalesDatePredictionProject.Server.Dto
+{
+    public class EmployeeDto
+    {
+
+        public int EmpId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
---
+++ SalesDatePredictionProject.Server/Dto/ShipperDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesDatePredictionProject.Server.Dto
+{
+    public class ShipperDto
+    {
+        public int ShipperId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
